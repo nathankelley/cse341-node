1359079c0dd02eb4dcf1305ab869328660a7cee7
Nathan Kelley <kel18018@byui.edu>
2023-04-20 23:02:57 -0600

    Web server first commit

---
+++ static_file_server.js
@@ -0,0 +1,13 @@
+#!javascript
+
+/*var express = require('express'),
+    app = express();
+app.use(express.static(__dirname + '/public'));
+app.listen(8080);*/
+
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Suhnya Kelley\n');
+}).listen(8080);
+console.log('Server running on port 8080.');
